bb9ee285b2ba70b6c38979117d8a4b0f887ebce2
milosdjurica <milosdjurica13@gmail.com>
2023-10-24 01:28:15 +0200

    library, header, sidebar finished

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 type SidebarProps = {
   children: React.ReactNode;
@@ -35,7 +36,6 @@ export default function Sidebar({ children }: SidebarProps) {
   return (
     <div className="flex h-full ">
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
-        <Box>Sidebar navigation</Box>
         <Box>
           <div className="flex flex-col gap-y-4 px-5 py-4">
             {routes.map((item) => (
@@ -43,8 +43,11 @@ export default function Sidebar({ children }: SidebarProps) {
             ))}
           </div>
         </Box>
-        <Box className="overflow-y-auto h-full">Song library</Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
       </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 }
