723929f2233c7e65367c5ad6e335e8e98510b98d
milosdjurica <milosdjurica13@gmail.com>
2023-10-25 18:31:53 +0200

    song liking functionality with LikeButton component

--- src/components/SearchContent.tsx
+++ src/components/SearchContent.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Song } from "../../types";
 import MediaItem from "./MediaItem";
+import LikeButton from "./LikeButton";
 
 type SearchContentProps = {
   songs: Song[];
@@ -11,7 +12,7 @@ type SearchContentProps = {
 export default function SearchContent({ songs }: SearchContentProps) {
   if (songs.length === 0)
     return (
-      <div className="flex flex-col gap-y-2 w-full px-6 text-netural-400">
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
         No songs found!
       </div>
     );
@@ -20,10 +21,10 @@ export default function SearchContent({ songs }: SearchContentProps) {
     <div className="flex flex-col gap-y-2 w-full px-6">
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
-          <div>
+          <div className="flex-1">
             <MediaItem data={song} />
           </div>
-          {/* // TODO: Add like button here */}
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
